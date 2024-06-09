186b194f3fa605355a07a6cba64898b30dd6cab1
unbearablefate <tylov.ohno@gmail.com>
2024-06-09 19:00:11 +0900

    rpc_ft_k-fac

--- test.py
+++ test.py
@@ -1,12 +1,31 @@
-import kfac.mischief as mischief
 
-mischief.mischief_init(world_size=16, possible_disconnect_node=None,
-                           max_disconnect_iter=20, disconnect_ratio=0.2,
-                            max_disconnected_node_num=4,
-                           ddp_trigger=True, factor_comm_trigger=True, inverse_comm_trigger=True)
+import datetime
+import time
 
-for i in range(1000):
-    mischief.update_iter()
-    if mischief.ITER >= mischief.LAST_AVG_ITER + 10:
-        print(f"update avg at iter {mischief.ITER}")
-        mischief.LAST_AVG_ITER = mischief.ITER
+import torch.distributed as dist
+import kfac.rpc_distributed as rpc_distributed
+from kfac.preconditioner import KFACPreconditioner
+from my_module.custom_resnet import MLP
+import logging
+logging.basicConfig(level=logging.NOTSET)
+
+timeout = datetime.timedelta(seconds=30)
+dist.init_process_group("gloo", timeout=timeout)
+model = MLP(num_hidden_layers=1)
+preconditioner = KFACPreconditioner(model,update_factors_in_hook = False)
+rpc_communicator = rpc_distributed.KFacRPCCommunicator(world_size=dist.get_world_size(), rank=dist.get_rank(), preconditioner=preconditioner)
+rpc_distributed.global_communicator = rpc_communicator
+rank = dist.get_rank()
+world_size = dist.get_world_size()
+for t in range(2):
+    rpc_communicator.update_self_t()
+    rpc_communicator.send_kfac_factor((rank +t)%world_size,"layers.1","A")
+    rpc_communicator.send_kfac_eigen_tensor("layers.1","G")
+    time.sleep(1)
+    if rank == 0:
+        print(repr(rpc_communicator))
+time.sleep(3)
+if rank == 0:
+    print(repr(rpc_communicator))
+rpc_communicator.shutdown()
+dist.destroy_process_group()
