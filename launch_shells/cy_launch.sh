407ef791a599ced20d975f7bd093ac0c521045a4
unbearablefate <tylov.ohno@gmail.com>
2024-08-05 00:32:54 +0900

    test

---
+++ launch_shells/cy_launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -A NBB
+#PBS -q gpu
+#PBS -T openmpi
+#PBS -b 32
+#PBS -l elapstim_req=00:10:00
+#PBS -v NQSV_MPI_VER=gdr/4.1.6/gcc8.3.1-cuda12.3.2
+#PBS -M kanakawapanman@gmail.com
+
+module load openmpi/gdr/4.1.6/gcc8.3.1-cuda12.3.2
+moudle load python/3.8
+source /work/NBB/yu_mingzhe/basicenv/bin/activate
+
+current_time=$(date "+%Y%m%d%H%M")
+
+mpirun -x PATH -np 4 --map-by ppr:1:node:PE=16 --report-bindings "$NQSII_MPIOPTS" \
+ /work/NBB/yu_mingzhe/basicenv/bin/python /work/NBB/yu_mingzhe/kfac-pytorch/mlp_fashion_minst.py \
+ --timestamp="$current_time"
