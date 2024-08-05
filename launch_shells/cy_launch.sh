5394f846ef408d604ade9569935b367bc754edd5
unbearablefate <tylov.ohno@gmail.com>
2024-08-05 15:27:51 +0900

    list

--- launch_shells/cy_launch.sh
+++ launch_shells/cy_launch.sh
@@ -13,6 +13,6 @@ module load openmpi/gdr/4.1.6/gcc8.3.1-cuda12.3.2
 
 current_time=$(date "+%Y%m%d%H%M")
 
-mpirun ${NQSII_MPIOPTS} -np 4 --map-by ppr:1:node:PE=16 --report-bindings \
+mpirun ${NQSII_MPIOPTS} -x PATH -np 4 --map-by ppr:1:node:PE=16 --report-bindings \
  /home/NBB/yu_mingzhe/miniconda3/envs/basic_env/bin/python /work/NBB/yu_mingzhe/kfac-pytorch/mlp_fashion_minst.py \
  --timestamp="$current_time"
