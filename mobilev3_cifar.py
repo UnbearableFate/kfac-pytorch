87932be10243ded3393cfa3acede1c0e65af85f4
unbearablefate <tylov.ohno@gmail.com>
2024-08-17 11:44:49 +0900

    test

--- mobilev3_cifar.py
+++ mobilev3_cifar.py
@@ -8,6 +8,10 @@ from my_module.custom_resnet import ResNetForCIFAR10, MLP ,SimpleCNN
 from general_util.GeneralManager import GeneralManager
 from my_module.mobile_net import CustomMiniMobileNetV3Small, CustomMobileNetV3Small
 from my_module.model_split import ModelSplitter
+from torchvision import transforms
+import torch.distributed as dist
+import logging
+
 
 gpu = torch.device("cuda:0")
 today = datetime.date.today().strftime('%m%d')
@@ -35,6 +39,8 @@ if DATA_DIR == "" or LOG_DIR == "" or Share_DIR == "":
 
 ompi_world_size = int(os.getenv('OMPI_COMM_WORLD_SIZE', -1))
 ompi_world_rank = int(os.getenv('OMPI_COMM_WORLD_RANK', -1))
+if ompi_world_rank == 0:
+    logging.basicConfig(level=logging.NOTSET)
 if __name__ == '__main__':
     print("Start!")
     timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M')
@@ -44,16 +50,30 @@ if __name__ == '__main__':
     timestamp = args.timestamp
     print(f"timestamp: {timestamp}")
 
+    timeout = datetime.timedelta(seconds=120)
+    dist.init_process_group("gloo", init_method=f"file://{Share_DIR}/pg_share{timestamp}", rank=ompi_world_rank,
+                            world_size=ompi_world_size, timeout=timeout)
+    if not dist.is_initialized():
+        raise RuntimeError("Unable to initialize process group.")
     model = CustomMobileNetV3Small(num_classes=10)
-    device = torch.device(f"cuda:{ompi_world_rank}")
+    device = torch.device(f"cuda:{ompi_world_rank%4}")
     model = model.to(device)
-    preconditioner = kfac.preconditioner.KFACPreconditioner(model=model, skip_layers=["block.0.0", "block.1.0"])
+    preconditioner = kfac.preconditioner.KFACPreconditioner(model=model, skip_layers=["block.0.0", "block.1.0"],damping=0.007, inv_update_steps=10)
 
-    data_path = DATA_DIR+str(ompi_world_rank)
-    mgr = GeneralManager(data_dir=DATA_DIR, dataset_name="CIFAR10", model=model,
+    transform = transforms.Compose([
+        transforms.Resize(224),  # 将图像大小调整为224x224
+        transforms.Grayscale(num_output_channels=3),  # 将灰度图像转换为3通道的RGB图像
+        transforms.ToTensor(),  # 将图像转换为张量，并且将像素值缩放到 [0, 1] 范围内
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],  # 对RGB通道进行标准化
+                             std=[0.229, 0.224, 0.225]),
+    ])
+
+    data_path = DATA_DIR + str(ompi_world_rank%8)
+    mgr = GeneralManager(data_dir=data_path, dataset_name="CIFAR10", model=model,
                          sampler_func= None,
-                         train_com_method='rpc', interval=1, is_2nd_order=True, epochs=20,device=device,
-                         share_file_path=Share_DIR,timestamp=timestamp, log_dir = LOG_DIR ,precondtioner=preconditioner)
+                         train_com_method='rpc', interval=13, is_2nd_order=True, epochs=5, device=device,
+                         share_file_path=Share_DIR, timestamp=timestamp, log_dir = LOG_DIR, precondtioner=preconditioner,
+                         transform_train=None, transform_test=None)
 
     mgr.rpc_train_and_test()
     mgr.close_all()
