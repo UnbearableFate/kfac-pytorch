8b0ec0d888be20309cb6876d2c29e0967a931cbc
unbearablefate <tylov.ohno@gmail.com>
2024-08-22 17:28:31 +0900

    test

--- run_local.sh
+++ run_local.sh
@@ -5,6 +5,6 @@ module load openmpi/gdr/4.1.6/gcc8.3.1-cuda12.3.2
 current_time=$(date "+%Y%m%d%H%M")
 
 mpirun ${NQSII_MPIOPTS} --mca mpi_abort_print_stack 1 \
-    -x PATH -np 4 --map-by ppr:4:node:PE=3 --nooversubscribe --report-bindings \
-    /home/NBB/yu_mingzhe/miniconda3/envs/basic_env/bin/python /work/NBB/yu_mingzhe/kfac-pytorch/mobilev3_cifar.py \
+    -x PATH -np 8 --report-bindings \
+    /home/NBB/yu_mingzhe/miniconda3/envs/basic_env/bin/python /work/NBB/yu_mingzhe/kfac-pytorch/mlp_fashion_minst.py \
     --timestamp="$current_time"
